4929be418c24514b51e87457c55820337957af0d
Robert Hambrock <roberthambrock@gmail.com>
2025-10-17 08:04:13 +0200

    basic nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Clojure development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "clojure-dev";
+          buildInputs = [
+            pkgs.clojure
+            pkgs.leiningen
+            pkgs.babashka
+          ];
+          shellHook = ''
+            echo "Welcome to the Clojure dev shell!"
+          '';
+        };
+      });
+}
